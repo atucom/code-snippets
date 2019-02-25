8294ed13cedc569e4a636c4c6590c294d916fda2
atucom <466817+atucom@users.noreply.github.com>
2019-02-25 08:38:34 -0700

    added embed() to ipython example

--- python/ipython_breakpoint.py
+++ python/ipython_breakpoint.py
@@ -5,3 +5,11 @@ import ipdb
 for i in range(1,10):
     ipdb.set_trace()
     print(i)
+
+
+#also, more useful sometimes:
+
+from IPython import embed
+for i in range(1,10):
+    embed()
+    print(i)
